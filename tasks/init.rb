21330a5e074c65ec2bf924d686ccdc5742e98af5
Jacob Wade <jacobmw@uber.com>
2018-01-22 15:25:57 -0800

    Much better now

--- tasks/init.rb
+++ tasks/init.rb
@@ -1,22 +1,37 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require 'facter'
 require 'json'
+require 'puppet'
 
-def update(excludes)
-  case Facter.value(:osfamily)
-  when 'Debian'
-    excludes.each do |exclude|
-      system("apt-mark hold #{exclude}")
-    end
-    system('apt-get upgrade -y')
-    excludes.each do |exclude|
-      system("apt-mark unhold #{exclude}")
-    end
-  when 'RedHat'
-    exclude = excludes.join(',')
-    command = "yum update --exclude=#{exclude} -y"
+def apt_hold(excludes)
+  excludes.each do |exclude|
+    @hold = "apt-mark hold #{exclude}"
+    stdout, stderr, status = Open3.capture3(@hold)
+    raise Puppet::Error, stderr if status != 0
+    { status: stdout.strip }
   end
-  system(command)
+end
+
+def apt_update
+  cmd_string = 'apt-get upgrade -y'
+  stdout, stderr, status = Open3.capture3(cmd_string)
+  raise Puppet::Error, stderr if status != 0
+  { status: stdout.strip }
+end
+
+def apt_unhold(excludes)
+  excludes.each do |exclude|
+    @hold = "apt-mark hold #{exclude}"
+    stdout, stderr, status = Open3.capture3(@hold)
+    raise Puppet::Error, stderr if status != 0
+    { status: stdout.strip }
+  end
+end
+
+def yum_update(excludes)
+  exclude = excludes.join(',') unless excludes.nil?
+  command = 'yum update -y'
+  command << "--exclude=#{exclude}" unless exclude.nil?
   stdout, stderr, status = Open3.capture3(command)
   raise Puppet::Error, stderr if status != 0
   { status: stdout.strip }
@@ -26,7 +41,17 @@ params = JSON.parse(STDIN.read)
 excludes = params['excludes']
 
 begin
-  result = update(excludes)
+  case Facter.value(:osfamily)
+  when 'Debian'
+    if excludes
+      result = apt_hold(excludes), apt_update + apt_unhold
+    else
+      result = apt_update
+    end
+  when 'RedHat'
+    result = yum_update(excludes)
+  end
+  puts result
   puts result.to_json
   exit 0
 rescue Puppet::Error => e
